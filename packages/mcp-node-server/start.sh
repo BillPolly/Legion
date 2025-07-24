eff89df8684b26fe790e1ffcdb2d0fc0cf0ccbf0
Bill234 <williammpearson@gmail.com>
2025-07-24 12:39:59 +0100

    tidy

--- packages/mcp-node-server/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting MCP Node Server..."
-echo "=========================="
-echo ""
-echo "The server will stay running until you press Ctrl+C"
-echo "You won't see a command prompt - this is normal!"
-echo ""
-exec npm start
