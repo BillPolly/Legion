d6ae5d8d3ba00faef1af156e4e5a195efef048ce
Bill234 <williammpearson@gmail.com>
2025-08-02 14:47:25 +0100

    deleted

--- packages/apps/aiur-actors-ui/src/components/base/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Base component exports for terminal UI
- */
-export { BaseModel } from './BaseModel.js';
-export { BaseView } from './BaseView.js';
-export { BaseViewModel } from './BaseViewModel.js';
-export { ExtendedBaseModel } from './ExtendedBaseModel.js';
-export { ExtendedBaseView } from './ExtendedBaseView.js';
-export { ExtendedBaseViewModel } from './ExtendedBaseViewModel.js';
