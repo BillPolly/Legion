25ed2926ddd6801f2a109f387d46d5398efd0bcd
Bill234 <williammpearson@gmail.com>
2025-08-10 19:27:44 +0100

    tidy

--- packages/tools/src/index.js
+++ packages/tools/src/index.js
@@ -11,36 +11,7 @@ export { Tool } from './modules/Tool.js';
 
 // Resource management  
 export { ResourceManager } from './ResourceManager.js';
-export { default as ResourceManagerDefault } from './ResourceManager.js';
 
-// Module loading system
-export { ModuleLoader } from './ModuleLoader.js';
-export { ModuleFactory } from './ModuleFactory.js';
-
-// Basic compatibility exports for tools-collection 
-export { ModuleInstance as Module } from './modules/ModuleInstance.js';
-
-// Simple ToolResult for compatibility
-export class ToolResult {
-  constructor(success, data, message) {
-    this.success = success;
-    this.data = data;
-    this.message = message;
-  }
-  
-  static success(data, message) {
-    return new ToolResult(true, data, message);
-  }
-  
-  static failure(message, data) {
-    return new ToolResult(false, data, message);
-  }
-}
-
-// Alias for easier migration
-export { ModuleInstance as BaseModule } from './modules/ModuleInstance.js';
-
-// Module implementations moved to @legion/tools-collection
 
 // Integration components
 export { ToolRegistry } from './integration/ToolRegistry.js';
