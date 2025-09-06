59c70e8c63aa04ba8720f4dc583bfda942778738
Bill234 <williammpearson@gmail.com>
2025-09-06 17:34:14 +0100

    Remove unused kg package
    - Deleted packages/kg directory and all its contents
    - Removed test:kg script from root package.json
    - The kg package was unused and replaced by a better implementation

--- packages/kg/__tests__/setup.js
+++
@@ -1,9 +0,0 @@
-/**
- * Jest setup for unit tests
- * ES6 JavaScript version
- */
-
-// Add any global test setup here
-// For example, you might want to set up global mocks or test utilities
-
-// Note: Jest timeout can be set in jest.config.js or individual tests
--- packages/kg/src-js/types/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Type exports for the Unified Capability Ontology
- * ES6 JavaScript version
- */
-
-export { Capability } from './Capability.js';
-export { ValidationResultBuilder } from './ValidationResult.js';
